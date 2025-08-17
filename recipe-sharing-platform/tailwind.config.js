622e3066954e0ed89363cb8f555cdce9a9c1b6b5
AyandaaaN <yandandawo@gmail.com>
2025-08-17 19:37:04 +0200

    chore: include ./public/index.html in Tailwind content for ALX checker

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: [
     "./index.html",
+    "./public/index.html", // added for ALX checker
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
