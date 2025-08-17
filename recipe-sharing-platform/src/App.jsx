ebb8124760cd28ca1c6ca41854cd841ee25429ff
AyandaaaN <yandandawo@gmail.com>
2025-08-17 19:19:40 +0200

    Initial commit: React + Tailwind setup

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is Working 🎉
+      </h1>
+      <p className="mt-2 text-gray-600">
+        If this heading is big and blue, your setup is 100% correct.
+      </p>
+    </div>
+  );
+}
